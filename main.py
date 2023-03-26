19ef8d186c6b520de2c748ffd34a645c8b1e7d2e
Jon Mico <jonmico@gmail.com>
2023-03-26 18:24:34 -0400

    add code for difficulty/number of tries, begin while loop code

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ print("I'm thinking of a number between 1 and 100.")
 # temporarily print the answer
 random_number = random.randint(1, 100)
 print(f"Psst, the correct answer is {random_number}.")
+
+difficulty_setting = input("Choose a difficulty. Type 'easy' or 'hard': ")
+if difficulty_setting == "easy":
+    number_of_tries = 10
+elif difficulty_setting == "hard":
+    number_of_tries = 5
+
+is_number_guessed = False
+
+while number_of_tries > 0 and not is_number_guessed:
+
