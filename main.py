355d03ef7b3191f1f1b9aa14c9ec904168ce87fc
Jon Mico <jonmico@gmail.com>
2023-03-26 18:35:40 -0400

    remove correct answer print, change some wording, final

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ print(logo)
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
-# temporarily print the answer
 random_number = random.randint(1, 100)
-print(f"Psst, the correct answer is {random_number}.")
+# temporarily print the answer
+# print(f"Psst, the correct answer is {random_number}.")
 
 difficulty_setting = input("Choose a difficulty. Type 'easy' or 'hard': ")
 if difficulty_setting == "easy":
@@ -18,7 +18,7 @@ elif difficulty_setting == "hard":
 is_number_guessed = False
 
 while number_of_tries > 0 and not is_number_guessed:
-    print(f"You have {number_of_tries} left.")
+    print(f"You have {number_of_tries} tries remaining to guess the number.")
     guess = int(input("Make a guess: "))
     if guess < random_number:
         print("Too low.")
