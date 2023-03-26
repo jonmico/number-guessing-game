74a64ef6384a259618d66f8e3b209c6ba5c639a0
Jon Mico <jonmico@gmail.com>
2023-03-26 19:32:44 -0400

    add ran out of attempts, fix bugs

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 random_number = random.randint(1, 100)
 # temporarily print the answer
-# print(f"Psst, the correct answer is {random_number}.")
+print(f"Psst, the correct answer is {random_number}.")
 
 difficulty_setting = input("Choose a difficulty. Type 'easy' or 'hard': ")
 if difficulty_setting == "easy":
@@ -22,12 +22,16 @@ while number_of_tries > 0 and not is_number_guessed:
     guess = int(input("Make a guess: "))
     if guess < random_number:
         print("Too low.")
-        print("Guess again.")
         number_of_tries -= 1
     elif guess > random_number:
         print("Too high.")
-        print("Guess again.")
         number_of_tries -= 1
     elif guess == random_number:
         print(f"You got it! The answer was {random_number}!")
         is_number_guessed = True
+
+    if number_of_tries > 0 and not is_number_guessed:
+        print("Guess again.")
+    elif number_of_tries == 0 and not is_number_guessed:
+        #
+        print(f"You ran out of attempts! You lose. The number was {random_number}.")
