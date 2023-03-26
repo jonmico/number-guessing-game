8f0e945596ee2cc0dd5f2dd6d76fee7eb821584c
Jon Mico <jonmico@gmail.com>
2023-03-26 18:20:24 -0400

    add random library and generate num

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from art import logo
+import random
 
 print(logo)
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 # temporarily print the answer
+random_number = random.randint(1, 100)
+print(f"Psst, the correct answer is {random_number}.")
